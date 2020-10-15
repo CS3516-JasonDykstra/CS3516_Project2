dced5b7d1a2b009440a5d5dd61fccd71b0302bea
JasonDykstra <jasondykstra2000@gmail.com>
2020-10-15 12:30:41 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/NetworksProject2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworksProject2.dir/Project2/project2.c.o"
+  "CMakeFiles/NetworksProject2.dir/Project2/student2.c.o"
+  "NetworksProject2"
+  "NetworksProject2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NetworksProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
